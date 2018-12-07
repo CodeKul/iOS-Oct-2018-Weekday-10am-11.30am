d1e2f8b3c1bca767eeca3137a76035052bb31807
Varun <varun.kudalkar@gmail.com>
2018-12-07 11:57:15 +0530

    Autolayout Demo

---
+++ iOSDemos/AutoLayoutDemo1/AutoLayoutDemo1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutDemo1
+//
+//  Created by Apple on 07/12/18.
+//  Copyright © 2018 Codekul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
