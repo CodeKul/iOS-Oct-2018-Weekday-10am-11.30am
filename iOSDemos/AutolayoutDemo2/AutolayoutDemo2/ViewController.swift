230a04fbd7e99eb887e64655114779ac0c1eae8a
Varun <varun.kudalkar@gmail.com>
2018-12-13 11:48:52 +0530

    more autolayout

---
+++ iOSDemos/AutolayoutDemo2/AutolayoutDemo2/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  AutolayoutDemo2
+//
+//  Created by Apple on 11/12/18.
+//  Copyright © 2018 Codekul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
